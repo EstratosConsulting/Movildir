2c9728c88caddf4278be335134b3c288ad99abf0
Estratos Consulting <info@estratos.net>
2015-07-15 11:05:25 -0600

    Revert "set main card"
    This reverts commit 66ae0b9505bc7a40bca6d7d355cd457f47eec049.

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,11 +1,51 @@
 Ext.define('Movildir.view.Main', {
-  extend: 'Ext.Container',
-  xtype: 'main',
-  config: {
-    cls: 'default-bg',
-    layout: 'card',
-    items: [{
-      xtype: 'categories'
-    }]
-  }
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            
+            
+             { 
+                xclass:'Movildir.view.MainNav',
+            
+              },
+            
+             { 
+                xclass:'Movildir.view.CategoriesList.CategoriesNav',
+            
+              },
+            
+             { 
+                xclass:'Movildir.view.ListComments.CommentsVista',
+            
+              },
+            
+            
+            {
+                title: 'Bienvenido',
+                iconCls: 'home',
+
+                styleHtmlContent: true,
+                scrollable: true,
+
+                items: {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Bienvenido a Sencha Touch 2'
+                },
+
+                html: [
+                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
+                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
+                    "and refresh to change what's rendered here."
+                ].join("")
+            }
+        ]
+    }
 });
